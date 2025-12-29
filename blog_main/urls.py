d2f9615e97523ecb5e86fcd6e846acc239e45eb8
Hassan Shaikh <hassanshailh908@gmail.com>
2025-12-29 19:42:26 +0530

    login and logout function

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
-    path('<slug:slug>', BlogView.blog_page, name='blog_page'),
+    path('blogs/<slug:slug>', BlogView.blog_page, name='blog_page'),
     path('blogs/search/', BlogView.search, name='search'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/',views.logout, name='logout'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
